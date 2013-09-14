ff2e858bb2f867cde89db72d598a91450c43d177
namagi <br.mladen@gmail.com>
2013-09-14 17:21:26 +0200

    Add custom sounds
    Change-Id: I477cf8821df1f2ff0999d26498981853db586152

---
+++ data/sounds/AudioPackageCustom.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH:= frameworks/base/data/sounds/custom
+
+# Notifications
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/notifications/Bio.ogg:system/media/audio/notifications/Bio.ogg \
+	$(LOCAL_PATH)/notifications/Chimes_Synthetic.ogg:system/media/audio/notifications/Chimes_Synthetic.ogg \
+	$(LOCAL_PATH)/notifications/Doorbell_Synthetic.ogg:system/media/audio/notifications/Doorbell_Synthetic.ogg \
+	$(LOCAL_PATH)/notifications/Dramatic_Strings.ogg:system/media/audio/notifications/Dramatic_Strings.ogg \
+	$(LOCAL_PATH)/notifications/Glam_alert.ogg:system/media/audio/notifications/Glam_alert.ogg \
+	$(LOCAL_PATH)/notifications/Grass_Meadow.ogg:system/media/audio/notifications/Grass_Meadow.ogg \
+	$(LOCAL_PATH)/notifications/Micro_Sound.ogg:system/media/audio/notifications/Micro_Sound.ogg \
+	$(LOCAL_PATH)/notifications/Pure_Tone.ogg:system/media/audio/notifications/Pure_Tone.ogg \
+	$(LOCAL_PATH)/notifications/Pure_Vibe.ogg:system/media/audio/notifications/Pure_Vibe.ogg \
+	$(LOCAL_PATH)/notifications/Rain_Drops.ogg:system/media/audio/notifications/Rain_Drops.ogg \
+
+
+# Ringtones
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ringtones/Alert_Bright.ogg:system/media/audio/ringtones/Alert_Bright.ogg \
+	$(LOCAL_PATH)/ringtones/Alert_Natural.ogg:system/media/audio/ringtones/Alert_Natural.ogg \
+	$(LOCAL_PATH)/ringtones/Alert_Synthetic.ogg:system/media/audio/ringtones/Alert_Synthetic.ogg \
+	$(LOCAL_PATH)/ringtones/Bell_Synthetic.ogg:system/media/audio/ringtones/Bell_Synthetic.ogg \
+	$(LOCAL_PATH)/ringtones/Bio_Research.ogg:system/media/audio/ringtones/Bio_Research.ogg \
+	$(LOCAL_PATH)/ringtones/Bits.ogg:system/media/audio/ringtones/Bits.ogg \
+	$(LOCAL_PATH)/ringtones/Bright.ogg:system/media/audio/ringtones/Bright.ogg \
+	$(LOCAL_PATH)/ringtones/Chimes_Synthetic.ogg:system/media/audio/ringtones/Chimes_Synthetic.ogg \
+	$(LOCAL_PATH)/ringtones/Chrome_Ring.ogg:system/media/audio/ringtones/Chrome_Ring.ogg \
+	$(LOCAL_PATH)/ringtones/Doorbell_Synthetic.ogg:system/media/audio/ringtones/Doorbell_Synthetic.ogg \
+	$(LOCAL_PATH)/ringtones/Provincial_Synthetic.ogg:system/media/audio/ringtones/Provincial_Synthetic.ogg \
+	$(LOCAL_PATH)/ringtones/Pure_Tone.ogg:system/media/audio/ringtones/Pure_Tone.ogg \
+	$(LOCAL_PATH)/ringtones/Pure_Vibe.ogg:system/media/audio/ringtones/Pure_Vibe.ogg \
